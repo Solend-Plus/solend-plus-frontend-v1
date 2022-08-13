4b6b20f1b2be81beb336c5c4fd96f8e9271c24cc
Mahdi Mostafavi <mmostafavi247@gamil.com>
2022-08-13 15:11:59 +0430

    Add structure of dashboard

---
+++ pages/apy/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import { Box } from "@mui/material";
+import Link from "next/link";
+
+const Dashboard: NextPage = ({}) => {
+  return <Box>dashboard</Box>;
+};
+
+export default Dashboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from "next";
 import { Box } from "@mui/material";
+import Link from "next/link";
 
 const Home: NextPage = ({}) => {
-  return <Box>hello!</Box>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "20rem",
+        pt: 6,
+        color: "black",
+      }}
+    >
+      <Link href="/apy/dashboard">APY history dashboard</Link>
+    </Box>
+  );
 };
 
 export default Home;
